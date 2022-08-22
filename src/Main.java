d45fa9251a04c71b546f9e732a5f1829d29ecc4b
IrinaKimeisha <irinkakim777@gmail.com>
2022-08-22 11:52:50 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,15 @@ public class Main {
          Реализовать метод подсчета площади для каждого типа фигуры и подсчет периметра.
          Создать массив из 5 фигур.
          Вывести на экран сумму периметра всех фигур в массиве. */
-        Figure[] figure = new Figure[3];
+        Figure[] figure = new Figure[5];
         figure[0] = new Triangle();
         figure[1] = new Circle();
         figure[2] = new Kvadrat();
+        figure[3] = new Kvadrat();
+        figure[4] = new Circle();
         double summaPerimetrov = 0;
         for (int i = 0; i < figure.length; i++) {
-            summaPerimetrov = figure[0].perimetrToFind() + figure[1].perimetrToFind() + figure[2].perimetrToFind();
+            summaPerimetrov += figure[i].perimetrToFind();
         }
         System.out.print("Сумма периметра всех фигур в массиве равна " + summaPerimetrov);
     }
